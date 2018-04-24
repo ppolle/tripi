1236caa7c9c52c17b39eaf757d57cb5981033837
MaryAnne45 <mary.jereh@gmail.com>
2018-04-24 16:18:42 +0300

    made changes to app.models.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,26 @@
 from flask import render_template,request,redirect,url_for
 from . import main
+from .. import db
+from ..models import Category,Event
 
 # Views
 @main.route('/')
 def index():
-	title = 'Flask Base'
-	return render_template('index.html',title = title)
+	title = 'Tripi'
+	category=Category.query.all()
+	events=Event.query.all()
+	return render_template('index.html',title = title,category=category,event=event)
+'''
+a view function that displays a specific category and its various events
+'''
+@main.route('/category/<int:id>',methods=["GET","POST"])
+def category(id):
+	categories=Category.query.filter_by(id=id).first()
+
+	events=Event.query.filter_by(category_id=id).all()
+	return render_template('main/category.html',categories=categories,events=events)
+@main.route('/Event/<int:id>')
+def event(id):
+	title='Available Events'
+	trips=Event.query.filter_by(id=id).first()
+	return render_template('main/events.html',title=title,trips=trips)
--- app/models.py
+++ app/models.py
@@ -1,19 +1,19 @@
-from . import login_manager
+from . import login_manager,db
 from flask_login import UserMixin
 from datetime import datetime
 
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-	name = db.Column(db.String(255))
-	email = db.Column(db.String(255))
-	pass_secure = db.Column(db.String(255))
-	creatorEvents = db.relationship('Event',backref = 'role',lazy = 'dynamic')
-	joinerEvents = db.relationship('Event',backref = 'role',lazy = 'dynamic')
+    id=db.Column(db.Integer,primary_key = True)
+    name=db.Column(db.String(255))
+    email=db.Column(db.String(255))
+    pass_secure=db.Column(db.String(255))
+    creatorEvents = db.relationship('Event',backref = 'role',lazy = 'dynamic')
+    joinerEvents = db.relationship('Event',backref = 'role',lazy = 'dynamic')
 
-	def __repr__(self):
-		return f'User {self.name}'
+    def __repr__(self):
+        return f'User {self.name}'
 
 class Event(db.Model):
 	__tablename__ = 'events'
@@ -21,11 +21,11 @@ class Event(db.Model):
 	name = db.Column(db.String)
 	location = db.Column(db.String)
 	p_count = db.Column(db.Integer)
-	event_date = db.Column(db.Datetime)
+	event_date = db.Column(db.DateTime,default=datetime.utcnow)
 	creator_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 	joiner_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 	category_id = db.Column(db.Integer,db.ForeignKey('events.id'))
-	
+
 
 	def __repr__(self):
 		return f'Event {self.name}'
