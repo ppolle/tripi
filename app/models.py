ef40fe56cadda6fd109860781ff4173db0147d4f
Peter Polle <peter.m.polle@gmail.com>
2018-04-24 12:24:04 +0300

    created User model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,26 @@
 from . import login_manager
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+	name = db.Column(db.String(255))
+	email = db.Column(db.String(255))
+	pass_secure = db.Column(db.String(255))
+	creatorEvents = db.relationship('Event',backref = 'role',lazy = 'dynamic')
+	joinerEvents = db.relationship('Event',backref = 'role',lazy = 'dynamic')
+
+	def __repr__(self):
+		return f'User {self.name}'
+
+class Event(db.Model):
+	__tablename__ = 'events'
+	id = db.Column(db.Integer,primaru_key = True)
+	name = db.Column(db.String)
+	location = db.Column(db.String)
+	p_count = db.Column(db.Integer)
+	event_date = db.Column(db.Datetime)
+	creator_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+	joiner_id = db.Column(db.Integer,db.ForeignKey('users.id'))
